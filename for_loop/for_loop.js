b42c165a15e91c10728d2d1aceedd8f88c2cfad4
LalithK90 <asakahatapitiya@gmail.com>
2020-11-26 13:17:44 +0530

    For loop

---
+++ for_loop/for_loop.js
@@ -0,0 +1,11 @@
+let cars = ['BMW','Toyota','Micro'];
+
+for (let car of cars){
+    console.log(car);
+}
+
+let text = "Hello-World";
+
+for (let textElement of text) {
+    console.log(textElement)
+}
