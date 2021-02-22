d5fe10ade349618a1f8b6caded2ebd5b74973faa
LalithK90 <asakahatapitiya@gmail.com>
2021-02-22 17:29:10 +0530

    Node module

---
+++ node_js/node_package_manager/index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+//for third way
+const fs = require("fs");
+
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((request, respose) => {
+    console.log("url "+request.url+" mehod "+ request.method);
+    respose.setHeader("Content-Type", "text/html");
+    let path = './';
+    switch (request.url) { 
+        case '/':
+            path += 'index.html';
+    respose.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+    respose.statusCode = 200;
+            break;
+        default:
+            respose.setHeader("Location", "/");
+    respose.statusCode = 301;
+            break;
+    }
+    fs.readFile(path, (err, data) => {
+        if (err) { 
+            console.error(err);
+            respose.end();
+        } else {
+            respose.end(data);
+        }
+    });
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Sever is runnig ${PORT}`);
+});
