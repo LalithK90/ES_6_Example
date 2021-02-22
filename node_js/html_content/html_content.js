d915398e0a37f798eb961b2cc03af37741ccf3c3
LalithK90 <asakahatapitiya@gmail.com>
2021-02-22 17:19:13 +0530

    Html Content passing, we status code and request redirect is ok

--- node_js/html_content/html_content.js
+++ node_js/html_content/html_content.js
@@ -1,10 +1,74 @@
 const http = require("http");
+//for third way
+const fs = require("fs");
 
-http.createServer((request, respose) => {
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+const PORT = process.env.PORT || 3000;
+//one way write html one browser
+// const server = http.createServer((request, respose) => {
+//     respose.statusCode = 200;
+//     respose.setHeader("Content-Type", "text/plain");
+//     respose.write("sapme")
+//     respose.end("The end");
+// });
+
+//two way write html one browser
+// const server = http.createServer((request, respose) => {
+//     respose.statusCode = 200;
+//     respose.setHeader("Content-Type", "text/html");
+//     respose.end(`<h2>Sample Header</h2>`);
+// });
+
+// server.listen(PORT, HOSTNAME, () => {
+//     console.log(`Sever is runnig ${PORT}`);
+// });
+
+//three way write html one browser
+// const server = http.createServer((request, respose) => {
+//     console.log("url "+request.url+" mehod "+ request.method);
+//     respose.statusCode = 200;
+//     respose.setHeader("Content-Type", "text/html");
+//     fs.readFile("./index.html", (err, data) => {
+//         if (err) { 
+//             console.error(err);
+//             respose.end();
+//         } else {
+//             respose.end(data);
+//         }
+//     });
+// });
+
+// server.listen(PORT, HOSTNAME, () => {
+//     console.log(`Sever is runnig ${PORT}`);
+// });
+
+//three way write html one browser according to request (routing)
+const server = http.createServer((request, respose) => {
+    console.log("url "+request.url+" mehod "+ request.method);
     respose.statusCode = 200;
-    respose.setHeader("Content-Type", "text/plain");
-    respose.write('Some Html ');
-    respose.end();
-}).listen(3000, () => {
-console.log("Server runnig ......");    
+    respose.setHeader("Content-Type", "text/html");
+    let path = './';
+    switch (request.url) { 
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+    fs.readFile(path, (err, data) => {
+        if (err) { 
+            console.error(err);
+            respose.end();
+        } else {
+            respose.end(data);
+        }
+    });
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Sever is runnig ${PORT}`);
 });
---
+++ node_js/web_status_code.js
@@ -0,0 +1,38 @@
+const http = require("http");
+//for third way
+const fs = require("fs");
+
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((request, respose) => {
+    console.log("url "+request.url+" mehod "+ request.method);
+    respose.setHeader("Content-Type", "text/html");
+    let path = './html_content/';
+    switch (request.url) { 
+        case '/':
+            path += 'index.html';
+    respose.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+    respose.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+    respose.statusCode = 404;
+            break;
+    }
+    fs.readFile(path, (err, data) => {
+        if (err) { 
+            console.error(err);
+            respose.end();
+        } else {
+            respose.end(data);
+        }
+    });
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Sever is runnig ${PORT}`);
+});
